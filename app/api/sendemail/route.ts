5b13abe581c61a145d3bcbeec7ee63d0e113ab88
Lakshita Joshi <119340612+lakshita1529@users.noreply.github.com>
2024-09-14 23:06:00 +0530

    update

--- app/api/sendemail/route.ts
+++ app/api/sendemail/route.ts
@@ -2,12 +2,15 @@ import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 import nodemailerExpressHandlebars from 'nodemailer-express-handlebars';
 import path from 'path';
+import emailAddresses from '../../../data/emails.json'; 
 
 const emailUser = process.env.EMAIL_USER!;
 const emailPass = process.env.EMAIL_PASS!;
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.zoho.in',
+  port: 465,
+  secure: true,
   auth: {
     user: emailUser,
     pass: emailPass,
@@ -28,25 +31,28 @@ transporter.use('compile', nodemailerExpressHandlebars(handlebarOptions));
 
 export async function POST(req: NextRequest) {
   try {
-    const { email, subject, message, user } = await req.json();
+    const { subject, message, user } = await req.json();
 
-    const mailOptions = {
-      from: `"admin" <${emailUser}>`,
-      to: email,
-      subject: subject, 
-      template: 'welcome', 
-      context: {
-        user,
-        message, 
-      },
-    };
+    const emailPromises = emailAddresses.map((email: string) => {
+      const mailOptions = {
+        from: `"admin" <${emailUser}>`,
+        to: email,
+        subject: subject,
+        template: 'welcome',
+        context: {
+          user,
+          message,
+        },
+      };
 
-    // Send the email function
-    await transporter.sendMail(mailOptions);
+      return transporter.sendMail(mailOptions);
+    });
 
-    return NextResponse.json({ message: 'Email sent successfully!' });
+    await Promise.all(emailPromises);
+
+    return NextResponse.json({ message: 'Emails sent successfully!' });
   } catch (error) {
-    console.error('Error sending email:', error);
-    return NextResponse.json({ message: 'Error sending email' }, { status: 500 });
+    console.error('Error sending emails:', error);
+    return NextResponse.json({ message: 'Error sending emails' }, { status: 500 });
   }
 }
